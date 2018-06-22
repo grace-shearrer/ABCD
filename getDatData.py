109f4e4c0383e3032567c07b61e7e52958fb07b4
gracer <gracer@gracers-imac.wireless-1x.unc.edu>
2018-06-22 13:35:13 -0400

    got it unzipiing

--- getDatData.py
+++ getDatData.py
@@ -12,20 +12,57 @@ import glob
 from nda_aws_token_generator import *
 import awsdownload
 import subprocess
+import argparse
 
-colnames = ['sub', 'scan']
 
-basedir = '/Users/gracer/Google Drive/ABCD/important_txt/female_scans'
-
-for txtfile in glob.glob(os.path.join(basedir,'*.csv')):
+def getDatData(colnames, basedir,arglist, password, downloaddir,checkdir):
+    print(basedir)
+    txtfile = os.path.join(basedir, 'fmriresults01.csv')
     infile = pd.read_csv(txtfile, names=colnames)
-    scans = infile.scan.tolist()
-    print(scans)
+    scans = infile.derived_files[1:5].tolist()
+    print(len(scans))
     for item in scans:
-        call = "python awsdownload.py %s -r -u args['WHO'] -p args['SECRET']"
-        subprocess.call
+        call = "python awsdownload.py %s -r %s -u %s -p %s"%(item, checkdir, arglist['WHO'],password)
+        call = call.split(' ')
+        print(call)
+        subprocess.call(call)
+    for filie in glob.glob(os.path.join(checkdir,'*.tgz')):
+        print('unzipping')
+        print(filie)
+        unzip = "tar -xvzf %s -C %s"%(filie,downloaddir)
+#        unzip = unzip.split(' ')
+        print('unzipping')
+        subprocess.call(unzip, shell = True)
+    for root, dirs, files in os.walk(os.path.join(downloaddir, 'submission_14921')):
+        print(root)
+        print(dirs)
+        print(files)
 #names = data.name.tolist()
 #latitude = data.latitude.tolist()
 #longitude = data.longitude.tolist()
         
 # make main and func, have to pass username and password
+            
+def Main():
+    password = input("enter password: ")
+    password  = str(password)
+    colnames = ['src_subject_id	gender','img03_id	file_source','derived_files','scan_type',	'session_det']
+    basedir = '/Users/gracer/Google Drive/ABCD/important_txt/'
+    downloaddir ='/Users/gracer/Desktop'
+    checkdir = '/Users/gracer/AWS_downloads/submission_14921'
+    
+    parser=argparse.ArgumentParser(description='Getting Data')
+    parser.add_argument('-username',dest='WHO',
+                        default=False, help='NDSR username')
+    
+    
+    args = parser.parse_args()
+    arglist={}
+    for a in args._get_kwargs():
+        arglist[a[0]]=a[1]
+    print(arglist)
+    
+    #######################################
+    getDatData(colnames, basedir, arglist, password, downloaddir,checkdir)
+    #######################################
+Main()
