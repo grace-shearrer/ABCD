025d48eb3d3bea4bed1d3cd3c6b037cffb73b017
NIBL <nibl@nibl.unc.private>
2018-07-30 17:25:37 -0400

    working on it

---
+++ makinCorrelations.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 30 17:09:15 2018
+script to get ABCD data going
+@author: nibl
+"""
+
+def cor_maker(basedir):
+    for file in 
+    
+def main():
+    basedir = '/Volumes/ABCDrive1/submission_14921'   
+    workdir = '/Volumes/ABCDrive1/workit'
+    cor_maker(basedir)
+main()
