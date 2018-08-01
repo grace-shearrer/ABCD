29ae6c2eeb78f1f343712dbee381bf7a92e7e54c
NIBL <nibl@nibl.unc.private>
2018-08-01 13:59:42 -0400

    working on keeping the good deleting the useless

--- makinCorrelations.py
+++ makinCorrelations.py
@@ -8,6 +8,7 @@ import subprocess
 import glob
 import os
 import pdb
+import shutil
 
 def cor_maker(basedir, workdir):
     biglist=glob.glob(os.path.join(basedir,'*.tgz'))
@@ -17,6 +18,7 @@ def cor_maker(basedir, workdir):
         #check for the folder 
         folder = 'sub-%s'%(x)
         output = os.path.join(workdir, folder, 'derivative')
+        keep = os.path.join(workdir, folder, 'keep')
         
         if os.path.exists(os.path.join(workdir,folder)):
             print('FOLDER EXISTS')
@@ -29,8 +31,15 @@ def cor_maker(basedir, workdir):
                 print(restcall)
                 runrest = subprocess.Popen(restcall, shell = True)
                 runrest.wait()
-                pdb.set_trace()
-            continue
+                
+            
+            for thing in glob.glob(os.path.join(output, '*')):
+                if thing.endswith("r_matrix.nii.gz"):
+                    print(thing)
+                    thing2= thing.split('/')[7]
+                    keep = os.path.join(keep, thing)
+                    shutil.move(thing, keep)
+            pdb.set_trace()
         else:
             matching = [s for s in biglist if x in s]
             print(matching)
